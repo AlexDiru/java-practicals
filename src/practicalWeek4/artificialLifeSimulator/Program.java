207057ebec7b9ad469bf962f071536bd3f1d0290
Alexander Spedding <battleprogrammeralex@gmail.com>
2012-11-01 14:03:44 +0000

    Final Week 4

--- src/practicalWeek4/artificialLifeSimulator/Program.java
+++ src/practicalWeek4/artificialLifeSimulator/Program.java
@@ -12,11 +12,11 @@ public class Program {
 		ABug bug2 = new ABug("Tom", 'T', 10);
 		ABug bug3 = new ABug("Snowy", 'S', 10);
 		
-		AWorld world = new AWorld(100,60);
+		AWorld world = new AWorld(20,12);
 		world.addBug(bug);
 		world.addBug(bug2);
 		world.addBug(bug3);
 		
-		world.main(3000);
+		world.main(5);
 	}
 }
